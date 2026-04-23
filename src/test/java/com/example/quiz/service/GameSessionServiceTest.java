6befea783232ff0d61310ea455cb4f3844b7af4b
fatmasboui <fatmasboui@esprit.tn>
2026-04-23 04:06:45 +0100

    Refactoring: SonarQube quality fixes and clean code

--- src/test/java/com/example/quiz/service/GameSessionServiceTest.java
+++ src/test/java/com/example/quiz/service/GameSessionServiceTest.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class GameSessionServiceTest {
+class GameSessionServiceTest {
 
     @Mock
     private GameSessionRepository repository;
--- src/test/java/com/example/quiz/service/ScoreServiceTest.java
+++ src/test/java/com/example/quiz/service/ScoreServiceTest.java
@@ -18,7 +18,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class ScoreServiceTest {
+class ScoreServiceTest {
 
     @Mock
     private ScoreRepository repository;
